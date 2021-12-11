e9ebb481fd88eb6f71fd128b614d5946ceb396bc
My-Ink <dev.myink@gmail.com>
2021-12-11 19:33:43 +0300

    Design Improvements

--- B_comp_weight.cpp
+++ B_comp_weight.cpp
@@ -5,8 +5,6 @@ namespace graph {
 
 class DSU {
 public:
-    using Item = size_t;
-
     DSU() : pres_(), rank_(), size_(0) {
     }
 
@@ -14,34 +12,38 @@ public:
         pres_.reserve(init_size);
         rank_.reserve(init_size);
         for (size_t i = 0; i < init_size; ++i) {
-            MakeSet();
+            AddSingleElementSet();
         }
     }
 
-    void MakeSet() {
+    void AddSingleElementSet() {
         pres_.push_back(pres_.size());
         rank_.push_back(1);
         ++size_;
     }
 
-    Item FindSet(Item x) {
-        if (pres_[x] == x) {
-            return x;
+    size_t FindSet(size_t item_id) {
+        if (pres_[item_id] == item_id) {
+            return item_id;
         }
-        return pres_[x] = FindSet(pres_[x]);
+        return pres_[item_id] = FindSet(pres_[item_id]);
     }
 
-    void Union(Item x, Item y) {
-        auto[x_set, y_set] = std::make_pair(FindSet(x), FindSet(y));
-        if (x_set == y_set) {
+    bool IsInSameSet(size_t item1_id, size_t item2_id) {
+        return FindSet(item1_id) == FindSet(item2_id);
+    }
+
+    void Unite(size_t item1_id, size_t item2_id) {
+        auto[set1, set2] = std::make_pair(FindSet(item1_id), FindSet(item2_id));
+        if (set1 == set2) {
             return;
         }
-        if (rank_[x_set] < rank_[y_set]) {
-            pres_[x_set] = y_set;
+        if (rank_[set1] < rank_[set2]) {
+            pres_[set1] = set2;
         } else {
-            pres_[y_set] = x_set;
-            if (rank_[x_set] == rank_[y_set]) {
-                ++rank_[x_set];
+            pres_[set2] = set1;
+            if (rank_[set1] == rank_[set2]) {
+                ++rank_[set1];
             }
         }
         --size_;
@@ -52,12 +54,37 @@ public:
     }
 
 private:
-    std::vector<Item> pres_;
+    std::vector<size_t> pres_;
     std::vector<size_t> rank_;
     size_t size_;
 };
 
 }  // namespace graph
+
+template <class Weight = size_t>
+class WeightedForest {
+public:
+    explicit WeightedForest(size_t n_trees) : dsu_(n_trees), weights_(n_trees, 0) {
+    }
+
+    Weight GetTreeWeight(size_t item_id) {
+        return weights_[dsu_.FindSet(item_id)];
+    }
+
+    void AddEdge(size_t src_id, size_t dst_id, Weight weight) {
+        if (dsu_.IsInSameSet(src_id - 1, dst_id - 1)) {
+            weights_[dsu_.FindSet(src_id - 1)] += weight;
+        } else {
+            auto new_weight = weights_[dsu_.FindSet(src_id - 1)] + weights_[dsu_.FindSet(dst_id - 1)] + weight;
+            dsu_.Unite(src_id - 1, dst_id - 1);
+            weights_[dsu_.FindSet(src_id - 1)] = new_weight;
+        }
+    }
+
+private:
+    graph::DSU dsu_;
+    std::vector<Weight> weights_;
+};
 
 using std::cin;
 using std::cout;
@@ -70,27 +97,21 @@ int main() {
     size_t n_requests = 0;
     cin >> n_vertices >> n_requests;
 
-    auto dsu = graph::DSU(n_vertices);
-    std::vector<size_t> comp_weights(n_vertices, 0);
+    auto forest = WeightedForest(n_vertices);
+
     int cmd = 0;
     for (size_t i = 0; i < n_requests; ++i) {
         cin >> cmd;
         if (cmd == 2) {
-            size_t x = 0;
-            cin >> x;
-            cout << comp_weights[dsu.FindSet(x - 1)] << '\n';
+            size_t item = 0;
+            cin >> item;
+            cout << forest.GetTreeWeight(item - 1) << '\n';
             continue;
         }
-        size_t from = 0;
-        size_t to = 0;
+        size_t src_id = 0;
+        size_t dst_id = 0;
         size_t weight = 0;
-        cin >> from >> to >> weight;
-        if (dsu.FindSet(from - 1) == dsu.FindSet(to - 1)) {
-            comp_weights[dsu.FindSet(from - 1)] += weight;
-        } else {
-            auto new_weight = comp_weights[dsu.FindSet(from - 1)] + comp_weights[dsu.FindSet(to - 1)] + weight;
-            dsu.Union(from - 1, to - 1);
-            comp_weights[dsu.FindSet(from - 1)] = new_weight;
-        }
+        cin >> src_id >> dst_id >> weight;
+        forest.AddEdge(src_id, dst_id, weight);
     }
 }
